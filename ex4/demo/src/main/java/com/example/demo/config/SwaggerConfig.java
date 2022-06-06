216188aeb42e0a2239f3fc248974301aae3a37c3
Athiwa <Athiwat_Jansomwong@cmu.ac.th>
2022-06-06 12:27:10 +0700

    edit pom profile

--- ex4/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ ex4/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 public class SwaggerConfig {
